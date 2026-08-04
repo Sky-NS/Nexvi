f99c2b3bc93b00774ad175d53013d06d94056ef6
Sky-NS <64040977+Sky-NS@users.noreply.github.com>
2026-08-04 22:22:44 +0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { HashRouter, Routes, Route } from 'react-router-dom';
+import { LanguageProvider } from '@/i18n/LanguageContext';
 import HomePage from '@/pages/HomePage';
 import TripWizard from '@/pages/TripWizard';
 import TripEditPage from '@/pages/TripEditPage';
@@ -6,16 +7,18 @@ import SettingsPage from '@/pages/SettingsPage';
 
 function App() {
   return (
-    <HashRouter>
-      <div className="min-h-screen bg-gray-50 text-gray-900">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/wizard" element={<TripWizard />} />
-          <Route path="/trip/:id" element={<TripEditPage />} />
-          <Route path="/settings" element={<SettingsPage />} />
-        </Routes>
-      </div>
-    </HashRouter>
+    <LanguageProvider>
+      <HashRouter>
+        <div className="min-h-screen bg-gray-50 text-gray-900">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/wizard" element={<TripWizard />} />
+            <Route path="/trip/:id" element={<TripEditPage />} />
+            <Route path="/settings" element={<SettingsPage />} />
+          </Routes>
+        </div>
+      </HashRouter>
+    </LanguageProvider>
   );
 }
 export default App;
