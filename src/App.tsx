f2988264d3cf6bc01639e7a4f095a84cbaf83682
Sky-NS <64040977+Sky-NS@users.noreply.github.com>
2026-08-05 03:47:53 +0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import { LanguageProvider } from '@/i18n/LanguageContext';
+import { useThemeEffect } from '@/hooks/useThemeEffect';
 import HomePage from '@/pages/HomePage';
 import TripWizard from '@/pages/TripWizard';
 import TripEditPage from '@/pages/TripEditPage';
 import SettingsPage from '@/pages/SettingsPage';
 
 function App() {
+  useThemeEffect();
+
   return (
     <LanguageProvider>
       <HashRouter>
